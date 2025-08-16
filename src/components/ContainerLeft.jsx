a702fcf38189a6de8b35fd58cd9187cab62ee0c2
mr-n30 <mr-n30@localhost>
2025-08-16 00:06:57 -0500

    Finished project

--- src/components/ContainerLeft.jsx
+++ src/components/ContainerLeft.jsx
@@ -5,7 +5,6 @@ export default function ContainerLeft({ input, setInput, setOutput, checked, set
 
     const handleInput = (e) => {
         setInput(e.target.value)
-        console.log(input)
     }
 
     const handleButtonClick = () => {
@@ -30,8 +29,9 @@ export default function ContainerLeft({ input, setInput, setOutput, checked, set
             <p className="title-text">Base64</p>
             <p className="title-text">Encode / Decode</p>
             <p className="title-text">Online</p>
+            <div className='created-by-container'><span className='created-by-text'>Created by: </span><a className='author-text' href="https://github.com/mr-n30">@mr-n30</a><img src="https://github.com/favicon.ico" /></div>
             <label className="user-input">
-                <textarea onChange={handleInput} id="user-input" name="user_input" value={input} className="user-input" placeholder={"Enter data to base64 " + ((checked) ? "encode" : "decode") + " here..."}></textarea>
+                <textarea onChange={handleInput} id="user-input" name="user_input" value={input} className="textarea-left" placeholder={"Enter data to base64 " + ((checked) ? "encode" : "decode") + " here..."}></textarea>
             </label>
             <div className="button-container">
                 <div className="encode-button-container">
@@ -46,7 +46,7 @@ export default function ContainerLeft({ input, setInput, setOutput, checked, set
                 <div className='dark-mode-button-container'>
                     <span className='dark-mode-text'>Dark Mode:</span>
                     <label className="switch">
-                        <input className="checkbox" type="checkbox" checked={isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} />
+                        <input className="checkbox" type="checkbox" checked={!isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} />
                         <span className="slider round"></span>
                     </label>
                 </div>
